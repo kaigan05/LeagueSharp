de3c45b39bdc712ad6cf7d33ccd5f7f97c1e62f8
kaigan05 <ntanphat2406@gmail.com>
2014-12-30 14:57:53 +0700

    Automatically Check for updates.

--- KaiHelper/GankDetector.cs
+++ KaiHelper/GankDetector.cs
@@ -44,14 +44,6 @@ namespace KaiHelper
                 {
                     _enemies.Add(hero, new Time());
                 }
-                //if (Helper.IsNewVersion(Helper.ReadFileFromUrl("https://raw.githubusercontent.com/kaigan05/LeagueSharp/master/KaiHelper/version.txt")))
-                //{
-                //    Console.WriteLine("KaiHelper version is old. Please check for updates!");
-                //}
-                Game.PrintChat("<font color = \"#00FF2B\">KaiHelper</font> by <font color = \"#FD00FF\">kaigan</font>");
-                Game.PrintChat(
-                    "<font color = \"#0092FF\">Feel free to donate via Paypal to:</font> <font color = \"#F0FF00\">ntanphat2406@gmail.com</font>");
-                Game.PrintChat("KaiHelper - Loaded");
             });
             Drawing.OnDraw += Drawing_OnDraw;
         }
--- KaiHelper/Program.cs
+++ KaiHelper/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using LeagueSharp;
 using LeagueSharp.Common;
 
 namespace KaiHelper
@@ -16,6 +17,14 @@ namespace KaiHelper
             new HealthTurret(Config);
             new JungleTimer(Config);
             Config.AddToMainMenu();
+            if (Helper.IsNewVersion(Helper.ReadFileFromUrl("https://raw.githubusercontent.com/kaigan05/LeagueSharp/master/KaiHelper/version.txt")))
+            {
+                Game.PrintChat("<font color = \"#ff002b\">KaiHelper version is old. Please check for updates!</font>");
+            }
+            Game.PrintChat("<font color = \"#00FF2B\">KaiHelper</font> by <font color = \"#FD00FF\">kaigan</font>");
+            Game.PrintChat(
+                "<font color = \"#0092FF\">Feel free to donate via Paypal to:</font> <font color = \"#F0FF00\">ntanphat2406@gmail.com</font>");
+            Game.PrintChat("KaiHelper - Loaded");
         }
     }
 }
